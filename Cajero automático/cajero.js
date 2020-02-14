653852c5d7fdf9b760fbbd8d7cfcb368fe9806b4
YTRodi <yagorodi@gmail.com>
2020-02-14 12:00:05 -0300

    cajero terminado

--- Cajero automático/cajero.js
+++ Cajero automático/cajero.js
@@ -1,6 +1,7 @@
 ///// Functions /////
 function deliverMoney()
 {
+    
     var txtBox = document.getElementById("dinero");// t = caja de texto.
     money = parseInt(txtBox.value);//con esto el dinero ya esta representado en pantalla.
 
@@ -41,14 +42,32 @@ function deliverMoney()
         //en cada ciclo muestro la cantidad de billetes correcta.
         for(var e of delivered) //e = instancia de la clase billete. //of =  voy a obtener una intancia de cada uno de los objetos.
         {
+            result.innerHTML += "Dinero retirado: <br/>";
             if(e.quanty > 0)
             {
+                
                 result.innerHTML +=  e.quanty + " billetes de $ " + e.value + "<br/>";
+                
                 //console.log(e);
                 e.showBilletes(); 
-            }
+                
+                //ULTIMO AGREGADO.
+                //result.appendChild(e.imagen);
+                totalDelivered = totalDelivered + (e.quanty * e.value);
+                console.log(totalDelivered);
+                available = available - totalDelivered;
+                //console.log(available);
+            
+            }   
+            e.quanty = 0;
+            totalDelivered = 0;
+            moneyAvailable.innerHTML = "El dinero disponible en el cajero es: " + available;
         }
-        
+        delivered = [];
+        if(available == 0)
+        {
+            alert("Usted ha retirado lo último que poseía el cajero, dirijase a la siguiente caja por favor.");
+        }
     }
 } 
 
@@ -62,8 +81,8 @@ imgOfBilletes["10"] = "billete10.png";
 var boxContainer = [];
 var delivered = [];
 //boxContainer.push(new Billete(100,5));
-boxContainer.push(new Billete(50,10)); //push le agrega nuevo objetos a nuestro array.
-boxContainer.push(new Billete(20,5)); 
+boxContainer.push(new Billete(50,35)); //push le agrega nuevo objetos a nuestro array.
+boxContainer.push(new Billete(20,8)); 
 boxContainer.push(new Billete(10,10));
 //boxContainer.push(new Billete(5,5)); 
 
@@ -76,4 +95,9 @@ button.addEventListener("click",deliverMoney);//entregar dinero.
 
 var result = document.getElementById("resultado");//resultado.
 
-
+//Ejercicio 2.
+var available = parseInt(prompt("Ingrese dinero disponible:"));//disponible
+var totalDelivered = 0;
+var initMoneyAvailable = document.getElementById("inicioCajeroDinero");
+initMoneyAvailable.innerHTML += "El cajero arrancó con $ " + available;
+var moneyAvailable = document.getElementById("dineroDisponibleParrafo");
